e3880709063a69702b4ace39120b5cb91a7a8a14
edenlecomte <eden.lecomte@ccc.govt.nz>
2016-12-08 15:31:05 +1300

    TFC API added

--- html/view1.php
+++ html/view1.php
@@ -11,3 +11,23 @@
           </div>
           <div class="square img_1-1">
           </div>
+
+<script>
+
+
+var date = new Date();
+var formattedDate = moment(date).format('YYYY-MM-DD');
+
+
+//grab TFC api data and save to var
+    $.ajax({
+        dataType: "json",
+        url: "http://api.tmpforchch.co.nz/v3/tmps?key=75565CFFEDC8FF92CF14744F4E47F&?=json&startdate=" + formattedDate + "&enddate=" + formattedDate + "&limit=99999",
+        contentType: 'text/plain',
+        success: function(data){
+            console.log(data);
+        }
+    });
+
+
+</script>
--- includes/scripts.php
+++ includes/scripts.php
@@ -1,1 +1,3 @@
+<script src="js/jquery-1.11.2.min.js"></script>
 <script src="js/jquery.mobile-1.4.5.min.js"></script>
+<script src="js/moment.js"></script>
