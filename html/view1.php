850941da3f214408960dececf407fd458b365da0
edenlecomte <eden.lecomte@ccc.govt.nz>
2016-12-08 16:46:22 +1300

    TFC API added

--- html/view1.php
+++ html/view1.php
@@ -1,16 +1,22 @@
-<h1>View 1</h1>
-          <div class="rect img_1-1">
-          </div>
-          <div class="square img_1-1">
-          </div>
-          <div class="square img_1-1">
-          </div>
-          <div class="square img_1-1">
-          </div>
-          <div class="rect2 img_1-1">
-          </div>
-          <div class="square img_1-1">
-          </div>
+<h4>TFC Data</h4>
+<div class="rect img_1-1">
+    <span id="tfcCount"></span>
+</div>
+<div class="square img_1-1">
+    <span id="tfcCount"></span>
+</div>
+<div class="square img_1-1">
+    <span id="tfcCount"></span>
+</div>
+<div class="square img_1-1">
+    <span id="tfcCount"></span>
+</div>
+<div class="rect2 img_1-1">
+    <span id="tfcCount"></span>   
+</div>
+<div class="square img_1-1">
+    <span id="tfcCount"></span>
+</div>
 
 <script>
 
@@ -18,16 +24,70 @@
 var date = new Date();
 var formattedDate = moment(date).format('YYYY-MM-DD');
 
+var tfcTile = $($('nav > ul > li')[0]).find('div');
+var tfcTiles = $('#view_1').find('div');
 
+//TFC Traffic variables
+var tfcData;
+
+var tfcCount,
+    tfcRoadWorks = [],
+    tfcRoadClosures = [],
+    tfcEvents = [];
 //grab TFC api data and save to var
+
+function updateTFC() {
     $.ajax({
         dataType: "json",
         url: "http://api.tmpforchch.co.nz/v3/tmps?key=75565CFFEDC8FF92CF14744F4E47F&?=json&startdate=" + formattedDate + "&enddate=" + formattedDate + "&limit=99999",
         contentType: 'text/plain',
         success: function(data){
             console.log(data);
+
+            tfcData = data.tmps;
+            tfcCount = tfcData.length;
+			for (i = 0; i < tfcData.length; i++) {
+                var currentData = tfcData[i];
+
+                //roadworks
+                if (currentData.jobtype == 'Road Works') {
+                    tfcRoadWorks.push(currentData);
+                };
+                if (currentData.jobtype == 'Event') {
+                    tfcEvents.push(currentData);
+                };        
+                if (currentData.roadclosurestatus == 'Road Closure') {
+                    tfcRoadClosures.push(currentData);
+                };      
+            };
+
+            //update tiles
+            var tfcHTML;
+            tfcHTML = '';
+            tfcHTML += '<h4>Traffic</h4>';
+            tfcHTML += '<p>Currently '+ tfcCount +' planned road works</p>';
+            $(tfcTile).html(tfcHTML);
+
+            //update subtiles
+            tfcHTML = '';
+            tfcHTML += '<h4>Road Works</h4>';
+            tfcHTML += '<p>'+ tfcRoadWorks.length +' planned Road Works</p>';
+            $(tfcTiles[0]).html(tfcHTML);
+
+            tfcHTML = '';
+            tfcHTML += '<h4>Events</h4>';
+            tfcHTML += '<p>'+ tfcEvents.length +' roads affected by events</p>';
+            $(tfcTiles[1]).html(tfcHTML);           
+
+            tfcHTML = '';
+            tfcHTML += '<h4>Roads Closed</h4>';
+            tfcHTML += '<p>'+ tfcRoadClosures.length +' roads closed</p>';
+            $(tfcTiles[2]).html(tfcHTML);                  
+
         }
     });
+}
+    
 
 
 </script>
--- includes/scripts.php
+++ includes/scripts.php
@@ -1,3 +1,16 @@
 <script src="js/jquery-1.11.2.min.js"></script>
-<script src="js/jquery.mobile-1.4.5.min.js"></script>
+<!--<script src="js/jquery.mobile-1.4.5.min.js"></script> -->
 <script src="js/moment.js"></script>
+
+
+<script>
+    //Dom ready
+    $(window).load( function() {
+        updateTFC();
+    });
+
+    //disable on click hash jumps
+    $($('nav > ul').find('a')).on('click', function() {
+        return false;
+    })
+</script>
